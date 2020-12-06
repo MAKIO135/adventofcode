a8f5f7cf12145314fab457e975014c4a2e2ea2c1
makio135 <makio135@gmail.com>
2020-12-06 09:17:14 +0100

    day 6 - 1

---
+++ 2020/6/1.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+
+fs.readFile('./input', 'utf8', (err, input) => {
+    if (err) throw err
+    // console.log(input)
+
+    const groups = input.split('\n\n').map(g => g.split('\n').join('').split(''))
+
+    const uniquesPerGroup = groups.map(g => [...new Set(g)].length)
+
+    const sum = uniquesPerGroup.reduce((acc, curr) => acc + curr)
+
+    console.log(sum)
+})
