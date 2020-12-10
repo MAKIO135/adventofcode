4c32d29949eda9abd61a4eae9245ce92baa0c5dd
makio135 <makio135@gmail.com>
2020-12-10 23:28:24 +0100

    update

--- 2020/10/1.js
+++ 2020/10/1.js
@@ -7,16 +7,12 @@ fs.readFile('./input', 'utf8', (err, input) => {
         .map(d => parseInt(d))
         .sort((a, b) => a < b ? -1 : 1)
         .reduce((acc, curr, i, arr) => {
-            const diff = i === 0 ? curr : curr-arr[i - 1]
+            const diff = i === 0 ? curr : curr - arr[i - 1]
             acc[`diff${diff}`]++
             return acc
-        }, {
-            diff1: 0,
-            diff3: 0
-        })
+        }, { diff1: 0, diff3: 0 })
     
     data.diff3++
     console.log(data)
-
     console.log(data.diff1 * data.diff3)
 })
