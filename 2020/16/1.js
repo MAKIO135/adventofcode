f7d317b9fd7847117cc622c21094d57a1b71fc36
makio135 <makio135@gmail.com>
2020-12-16 23:10:40 +0100

    day 16 - 1

---
+++ 2020/16/1.js
@@ -0,0 +1,24 @@
+const { clear, log } = require('console')
+clear()
+
+const fs = require('fs')
+
+fs.readFile('./input', 'utf8', (err, input) => {
+    if (err) throw err
+
+    let [rules, ticket, nearby] = input.split('\n\n').map(d => d.split('\n'))
+    ticket.shift()
+    nearby.shift()
+
+    rules = rules.map(rule => rule.match(/(\d+)-(\d+)/g).map(range => range.split('-').map(d => parseInt(d)))).flat()
+    log(rules)
+
+    nearby = nearby.map(t => t.split(',').map(d => parseInt(d))).flat()
+    log(nearby)
+
+    let errorRate = nearby
+        .filter(n => !rules.some(range =>  range[0] <= n && n <= range[1]))
+        .reduce((acc, cur) => acc + cur, 0)
+    
+    log({errorRate})
+})
