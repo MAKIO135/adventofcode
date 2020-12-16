1d552c3aa08a4d91192972f27ecf6216db3c2395
makio135 <makio135@gmail.com>
2020-12-17 00:13:53 +0100

    day 16 - 2

---
+++ 2020/16/2.js
@@ -0,0 +1,64 @@
+const { clear, log } = require('console')
+clear()
+
+const fs = require('fs')
+const { cursorTo } = require('readline')
+
+fs.readFile('./input', 'utf8', (err, input) => {
+    if (err) throw err
+
+    let [rules, ticket, nearby] = input.split('\n\n').map(d => d.split('\n'))
+    ticket.shift()
+    ticket = ticket[0].split(',').map(d => parseInt(d))
+    // log(ticket)
+    
+    rules = rules.map(rule => ({
+        name: rule.split(':')[0],
+        ranges: rule.match(/\d+-\d+/g).map(range => range.split('-').map(d => parseInt(d)))
+    }))
+    // log(rules)
+    
+    nearby.shift()
+    nearby = nearby.map(t => t.split(',').map(d => parseInt(d)))
+        .filter(range => range.every(n => rules.some(rule => rule.ranges.some(range =>  range[0] <= n && n <= range[1]))))
+    // log(nearby)
+    
+    const fixed = []
+    
+    const observe = () => {
+        rules.forEach(rule => {
+            let index = []
+            for(let i = 0; i < ticket.length; i ++) {
+                if([ticket, ...nearby].every(t => rule.ranges.some(range => range[0] <= t[i] && t[i] <= range[1]))) index.push(i)
+            }
+            rule.index = index.length === 1 ? index[0] : index
+        })
+        rules.forEach(rule => console.log(`${rule.name}: ${rule.index}`))
+    }
+
+    const propagate = () => {
+        for(let i = rules.length - 1; i >= 0; i--) {
+            const rule = rules[i]
+            if(typeof rule.index === 'number') fixed.push(...rules.splice(i, 1))
+        }
+        console.log(fixed.length, rules.length)
+        
+        rules.forEach(rule => {
+            fixed.forEach(({index}) => {
+                if(rule.index.indexOf(index) >= 0) {
+                    rule.index.splice(rule.index.indexOf(index), 1)
+                    if(rule.index.length === 1) rule.index = rule.index[0]
+                }
+            })
+        })
+    }
+
+    observe()
+    while(rules.length) propagate()
+    fixed.forEach(rule => console.log(`${rule.name}: ${rule.index}`))
+
+    const result = fixed.filter(({name}) => name.startsWith('departure'))
+        .reduce((acc, curr) => acc * ticket[curr.index], 1)
+    
+    log({ result })
+})
