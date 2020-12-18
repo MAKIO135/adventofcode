8ff4a8e13db1c68a7d6aff48d67754bf96c8c54f
makio135 <makio135@gmail.com>
2020-12-18 22:56:49 +0100

    update template

--- 2020/0/1.js
+++ 2020/0/1.js
@@ -1,9 +1,12 @@
 const fs = require('fs')
+const { clear, log } = require('console')
+clear()
+log('/'.repeat(100))
 
 fs.readFile('./test', 'utf8', (err, input) => {
     if (err) throw err
 
     input = input.split('\n')
     
-    console.log(input)
+    log(input)
 })
