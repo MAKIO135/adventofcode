a5ea9de805fb0858a8a7d13f42eef6edd3419535
makio135 <makio135@gmail.com>
2020-12-18 22:56:54 +0100

    18 clean

--- 2020/18/1.js
+++ 2020/18/1.js
@@ -1,55 +1,36 @@
+const fs = require('fs')
 const { clear, log } = require('console')
 clear()
 log('/'.repeat(100))
-
-const fs = require('fs')
 
 fs.readFile('./input', 'utf8', (err, input) => {
     if (err) throw err
 
     const computeGroup = g => {
-        let result = 0
-        let operator = '+'
+        let result = 0, operator = '+'
         
-        // remove parentheses
-        g = g.split('')
-        g.shift()
-        g.pop()
-        g = g.join('')
-
-        g = g.split(' ')
-
         g.forEach(c => {
             if(['+', '*'].includes(c)) operator = c
             else {
                 c = parseInt(c)
                 if(operator === '+') result += c
-                if(operator === '*') result *= c
+                else if(operator === '*') result *= c
             }
         })
 
         return result
     }
 
-    const results = input.split('\n').map(l => {
-        // add parentheses to consider the whole expression as a group
-        l = l.split('')
-        l.unshift('(')
-        l.push(')')
-        l = l.join('')
+    const sum = input.split('\n').map(l => {
+        l = `(${l})` // consider the whole expression as a group
 
         let groups = l.match(/\([0-9+*\s]+\)/g)
         while(groups && groups.length) {
-            groups.forEach(g => {
-                l = l.replace(g, computeGroup(g))
-            })
+            groups.forEach(g => l = l.replace(g, computeGroup(g.slice(1, -1).split(' '))))  // remove parentheses and split by spaces
             groups = l.match(/\([0-9+*\s]+\)/g)
         }
         
         return l
-    })
-    // log(results)
-
-    const sum = results.reduce((acc, curr) => acc + parseInt(curr), 0)
+    }).reduce((acc, curr) => acc + parseInt(curr), 0)
     log(sum)
 })
--- 2020/18/2.js
+++ 2020/18/2.js
@@ -1,57 +1,38 @@
+const fs = require('fs')
 const { clear, log } = require('console')
 clear()
 log('/'.repeat(100))
-
-const fs = require('fs')
 
 fs.readFile('./input', 'utf8', (err, input) => {
     if (err) throw err
 
     const computeGroup = g => {
-        // remove parentheses
-        g = g.split('')
-        g.shift()
-        g.pop()
-        g = g.join('')
-        
-        if(g.includes('+')) {
-            let groups = g.match(/\d+\s\+\s\d+/g)
-            while(groups && groups.length) {
-                g = g.replace(groups[0], eval(groups[0]))
-                groups = g.match(/\d+\s\+\s\d+/g)
-            }
+        let adds = g.match(/\d+\s\+\s\d+/g) // additions
+        while(adds && adds.length) {
+            adds.forEach(d => g = g.replace(d, eval(d)))
+            adds = g.match(/\d+\s\+\s\d+/g)
         }
 
-        if(g.includes('*')) {
-            let groups = g.match(/\d+\s\*\s\d+/g)
-            while(groups && groups.length) {
-                g = g.replace(groups[0], eval(groups[0]))
-                groups = g.match(/\d+\s\*\s\d+/g)
-            }
+        let mults = g.match(/\d+\s\*\s\d+/g) // multiplications
+        while(mults && mults.length) {
+            mults.forEach(d => g = g.replace(d, eval(d)))
+            mults = g.match(/\d+\s\*\s\d+/g)
         }
 
         return g
     }
 
-    const results = input.split('\n').map(l => {
-        // add parentheses to consider the whole expression as a group
-        l = l.split('')
-        l.unshift('(')
-        l.push(')')
-        l = l.join('')
+    const sum = input.split('\n').map(line => {
+        line = `(${line})` // consider the whole expression as a group
 
-        let groups = l.match(/\([0-9+*\s]+\)/g)
+        let groups = line.match(/\([0-9+*\s]+\)/g)
         while(groups && groups.length) {
-            groups.forEach(g => {
-                l = l.replace(g, computeGroup(g))
-            })
-            groups = l.match(/\([0-9+*\s]+\)/g)
+            groups.forEach(g => line = line.replace(g, computeGroup(g.slice(1, -1)))) // remove parentheses
+            groups = line.match(/\([0-9+*\s]+\)/g)
         }
 
-        return l
-    })
-    // log(results)
+        return line
+    }).reduce((acc, curr) => acc + parseInt(curr), 0)
 
-    const sum = results.reduce((acc, curr) => acc + parseInt(curr), 0)
     log(sum)
 })
