51994cf23b7a1f2dcff86fb9f788ecb41b2f911d
makio135 <makio135@gmail.com>
2020-12-21 20:53:15 +0100

    fix revert 18-2

--- 2020/18/2.js
+++ 2020/18/2.js
@@ -1,38 +1,45 @@
-const fs = require('fs')
 const { clear, log } = require('console')
 clear()
 log('/'.repeat(100))
+
+const fs = require('fs')
 
 fs.readFile('./input', 'utf8', (err, input) => {
     if (err) throw err
 
     const computeGroup = g => {
-        let adds = g.match(/\d+\s\+\s\d+/g) // additions
-        while(adds && adds.length) {
-            adds.forEach(d => g = g.replace(d, eval(d)))
-            adds = g.match(/\d+\s\+\s\d+/g)
+        g = g.replace(/\(|\)/g, '') // remove parentheses
+        
+        if(g.includes('+')) {
+            let groups = g.match(/\d+\s\+\s\d+/g)
+            while(groups && groups.length) {
+                g = g.replace(groups[0], eval(groups[0]))
+                groups = g.match(/\d+\s\+\s\d+/g)
+            }
         }
 
-        let mults = g.match(/\d+\s\*\s\d+/g) // multiplications
-        while(mults && mults.length) {
-            mults.forEach(d => g = g.replace(d, eval(d)))
-            mults = g.match(/\d+\s\*\s\d+/g)
+        if(g.includes('*')) {
+            let groups = g.match(/\d+\s\*\s\d+/g)
+            while(groups && groups.length) {
+                g = g.replace(groups[0], eval(groups[0]))
+                groups = g.match(/\d+\s\*\s\d+/g)
+            }
         }
 
         return g
     }
 
-    const sum = input.split('\n').map(line => {
-        line = `(${line})` // consider the whole expression as a group
+    const results = input.split('\n').map(l => {
+        l = `(${l})`// consider the whole expression as a group
 
-        let groups = line.match(/\([0-9+*\s]+\)/g)
+        let groups = l.match(/\([0-9+*\s]+\)/g)
         while(groups && groups.length) {
-            groups.forEach(g => line = line.replace(g, computeGroup(g.slice(1, -1)))) // remove parentheses
-            groups = line.match(/\([0-9+*\s]+\)/g)
+            groups.forEach(g => l = l.replace(g, computeGroup(g)))
+            groups = l.match(/\([0-9+*\s]+\)/g)
         }
 
-        return line
-    }).reduce((acc, curr) => acc + parseInt(curr), 0)
-
+        return l
+    })
+    const sum = results.reduce((acc, curr) => acc + parseInt(curr), 0)
     log(sum)
 })
