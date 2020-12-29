e9e47feb46dd84b10f3984b4789f4547940e4556
makio135 <makio135@gmail.com>
2020-12-29 23:04:00 +0100

    create template for all days for 2015

---
+++ 2015/25/2.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const { clear, log } = require('console')
+clear()
+log('/'.repeat(100))
+
+fs.readFile('./test', 'utf8', (err, input) => {
+    if (err) throw err
+
+    input = input.split('\n')
+    
+    log(input)
+})
