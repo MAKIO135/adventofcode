0ba09c50475e7d401f29011fc5b850747984d5fc
makio135 <makio135@gmail.com>
2020-12-30 10:43:01 +0100

    2015 - day 5

--- 2015/5/1.js
+++ 2015/5/1.js
@@ -1,12 +1,16 @@
 const fs = require('fs')
 const { clear, log } = require('console')
+const { strict } = require('assert')
 clear()
 log('/'.repeat(100))
 
-fs.readFile('./test', 'utf8', (err, input) => {
+fs.readFile('./input', 'utf8', (err, input) => {
     if (err) throw err
 
-    input = input.split('\n')
+    const niceStrings = input.split('\n')
+        .filter(d => d.match(/[aeiou]/g)?.length >= 3)
+        .filter(d => d.match(/([a-z])\1/g)?.length)
+        .filter(d => !d.match(/ab|cd|pq|xy/g)?.length)
     
-    log(input)
+    log(niceStrings.length)
 })
--- 2015/5/2.js
+++ 2015/5/2.js
@@ -1,12 +1,15 @@
 const fs = require('fs')
 const { clear, log } = require('console')
+const { strict } = require('assert')
 clear()
 log('/'.repeat(100))
 
-fs.readFile('./test', 'utf8', (err, input) => {
+fs.readFile('./input', 'utf8', (err, input) => {
     if (err) throw err
 
-    input = input.split('\n')
+    const niceStrings = input.split('\n')
+        .filter(d => d.match(/([a-z]{2})\w*\1/g)?.length)
+        .filter(d => d.match(/([a-z])\w{1}\1/g)?.length)
     
-    log(input)
+    log(niceStrings.length)
 })
