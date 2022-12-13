3530aa2cf8f51dd309ab07ea0ea145b82702b67c
makio135 <makio135@gmail.com>
2022-12-13 12:12:12 +0100

    2022-13

--- 2022/13/1.js
+++ 2022/13/1.js
@@ -3,10 +3,41 @@ const { clear, log } = require('console')
 clear()
 log('/'.repeat(100))
 
-fs.readFile('./test', 'utf8', (err, input) => {
+fs.readFile('./input', 'utf8', (err, input) => {
     if (err) throw err
+    const compare = (left, right, n) => {
+        // If both values are integers
+        if(Number.isInteger(left) && Number.isInteger(right)) {
+            return left === right ? 'next' : left < right ? true : false
+        }
 
-    input = input.split('\n')
+        // If a list runs out of items
+        if(left === undefined || right === undefined) {
+            return left === undefined && right === undefined ? 'next' :
+                left === undefined ? true :
+                false
+        }
+        
+        // If exactly one value is an integer
+        if(Number.isInteger(left) ^ Number.isInteger(right)) {
+            if(Number.isInteger(left)) left = [left]
+            else right = [right]
+        }
+
+        // If both values are lists
+        let result = 'next'
+        for(let i = 0; i < Math.max(left.length, right.length) && result === 'next'; i ++) {
+            result = compare(left[i], right[i], n+1)
+        }
+        return result
+    }
+
+    input = input.split('\n\n')
+        .map(d => d.split('\n').map(l => JSON.parse(l)))
+        .map(([left, right], i) => {
+            const result = compare(left, right, 0)
+            return result
+        })
     
-    log(input)
+    log(input.reduce((acc, d, i) => acc += d === true ? (i + 1) : 0, 0))
 })
--- 2022/13/2.js
+++ 2022/13/2.js
@@ -3,10 +3,46 @@ const { clear, log } = require('console')
 clear()
 log('/'.repeat(100))
 
-fs.readFile('./test', 'utf8', (err, input) => {
+fs.readFile('./input', 'utf8', (err, input) => {
     if (err) throw err
+    const compare = (left, right, n) => {
+        // If both values are integers
+        if(Number.isInteger(left) && Number.isInteger(right)) {
+            return left === right ? 'next' : left < right ? 1 : -1
+        }
 
-    input = input.split('\n')
+        // If a list runs out of items
+        if(left === undefined || right === undefined) {
+            return left === undefined && right === undefined ? 'next' :
+                left === undefined ? 1 :
+                -1
+        }
+        
+        // If exactly one value is an integer
+        if(Number.isInteger(left) ^ Number.isInteger(right)) {
+            if(Number.isInteger(left)) left = [left]
+            else right = [right]
+        }
+
+        // If both values are lists
+        let result = 'next'
+        for(let i = 0; i < Math.max(left.length, right.length) && result === 'next'; i ++) {
+            result = compare(left[i], right[i], n+1)
+        }
+        return result
+    }
+
+    let decoderKey = 1
+    ;[...input.split('\n\n'), '[[2]]\n[[6]]']
+        .map(d => d.split('\n').map(l => JSON.parse(l))).flat()
+        .sort((left, right) => compare(right, left))
+        .map(d => JSON.stringify(d))
+        .forEach((d, i) => {
+            if(['[[2]]','[[6]]'].includes(d)) {
+                decoderKey *= i + 1
+            }
+        })   
+
     
-    log(input)
+    log(decoderKey)
 })
