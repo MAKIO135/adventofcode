c50d260287bce63a06ed1d7e6d7ef5da114dc2e8
makio135 <makio135@gmail.com>
2023-12-05 14:31:02 +0100

    day 5

--- 2023/5/1.js
+++ 2023/5/1.js
@@ -3,10 +3,28 @@ const { clear, log } = require('console')
 clear()
 log('/'.repeat(100))
 
-fs.readFile('./test', 'utf8', (err, input) => {
+const extractNumbers = s => [...s.matchAll(/\d+/g)].map(d => parseInt(d[0]))
+
+fs.readFile('./input', 'utf8', (err, input) => {
     if (err) throw err
 
-    input = input.split('\r\n')
+    input = input.split('\n\n').map(p => p.split('\n').map((l, i) => i === 0 ? l : extractNumbers(l)))
+
+    const seeds = extractNumbers(input.shift()[0])
+
+    const maps = input.map(d => {
+        const name = d.shift()
+        const ranges = d.map(([destStart, srcStart, len]) => ({srcStart, destStart, len}))
+            .sort((a, b) => a.srcStart - b.srcStart)
+        return { name, ranges }
+    })
     
-    log(input)
+    const locations = seeds.map(s => {
+        return maps.reduce((acc, d) => {
+            const range = d.ranges.find(r => acc >= r.srcStart && acc < r.srcStart + r.len)
+            return range ? range.destStart + (acc - range.srcStart) : acc
+        }, s)
+    })
+
+    log(locations.sort((a,b) => a - b)[0])
 })
--- 2023/5/2.js
+++ 2023/5/2.js
@@ -3,10 +3,68 @@ const { clear, log } = require('console')
 clear()
 log('/'.repeat(100))
 
-fs.readFile('./test', 'utf8', (err, input) => {
+const extractNumbers = s => [...s.matchAll(/\d+/g)].map(d => parseInt(d[0]))
+
+fs.readFile('./input', 'utf8', (err, input) => {
     if (err) throw err
 
-    input = input.split('\r\n')
+    input = input.split('\n\n').map(p => p.split('\n').map((l, i) => i === 0 ? l : extractNumbers(l)))
+
+    const seeds = extractNumbers(input.shift()[0])
     
-    log(input)
+    const seedRanges = seeds.reduce((acc, d, i) => {
+        if(i % 2 === 1) acc.push([seeds[i-1], seeds[i-1]+d-1])
+        return acc
+    }, [])
+
+    const maps = input.map(d => {
+        const name = d.shift()
+        const ranges = d.map(([destStart, srcStart, len], i) => ({srcStart, srcEnd: srcStart + len - 1, destStart, destEnd : destStart + len - 1, len, i}))
+            .sort((a, b) => a.srcStart - b.srcStart)
+        return ranges
+    })
+
+    const passMap = (inputs, map) => {
+        const n = []
+        inputs.forEach(inputRange => inputRange.map(v => n.push({n : v})))
+        map.forEach(r => n.push(
+            {n : r.srcStart, r}, 
+            {n : r.srcEnd, r})
+        )
+        const sorted = n.sort((a, b) => (a.n - (a.r ? 1 : 0)) - (b.n - (b.r ? 1 : 0)))
+        
+        let outputs = []
+
+        let currentRange = undefined
+        let isIn = false
+        sorted.forEach(({n, r}, i) => {
+            if(r && n == r.srcStart) {
+                currentRange = r
+                if(isIn) {
+                    if(!sorted.some(d => d.n === n-1)) outputs.push(n-1)
+                    outputs.push(r.destStart)
+                }
+            }
+            if(r && n == r.srcEnd) {
+                currentRange = undefined
+                if(isIn) {
+                    outputs.push(r.destEnd)
+                    if(!sorted.some(d => d.n === n+1)) outputs.push(n+1)
+                }
+            }
+            
+            if(!r) {
+                isIn = !isIn
+                if(currentRange) outputs.push(currentRange.destStart + (n - currentRange.srcStart))
+                else outputs.push(n)
+            }
+        })
+
+        return outputs.reduce((acc, d, i) => {
+            if(i % 2) acc.push([outputs[i-1], d])
+            return acc
+        }, [])
+    }
+
+    log(maps.reduce((acc, map) => passMap(acc, map), seedRanges).flat().sort((a, b) => a - b)[0])
 })
